f1cc7478d162b2811b1369a093eb61a17ec343c9
renwenqiang <1225095213@qq.com>
2020-08-26 11:35:09 +0800

    init commit

---
+++ src/main/java/com/boot/springmybatismultipledb/SpringMybatisMultipledbApplication.java
@@ -0,0 +1,13 @@
+package com.boot.springmybatismultipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisMultipledbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisMultipledbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/springmybatismultipledb/SpringMybatisMultipledbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.springmybatismultipledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisMultipledbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
