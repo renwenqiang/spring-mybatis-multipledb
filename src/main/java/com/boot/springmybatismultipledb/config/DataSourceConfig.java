a376212d03b5d1af7a84fc762b6ddc7309c3f070
renwenqiang <1225095213@qq.com>
2020-08-27 15:35:15 +0800

    v0.2: with transcation

--- src/main/java/com/boot/springmybatismultipledb/config/DataSourceConfig.java
+++ src/main/java/com/boot/springmybatismultipledb/config/DataSourceConfig.java
@@ -1,42 +1,73 @@
 package com.boot.springmybatismultipledb.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.mysql.cj.jdbc.MysqlXADataSource;
+import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
-@Slf4j
+@MapperScan(basePackages = "com.boot.springmybatismultipledb.mapper", sqlSessionTemplateRef = "sqlSessionTemplate")
 public class DataSourceConfig {
+
 
     /**
      * 创建多个数据源 ds1 和 ds2
-     * 此处的Primary，是设置一个Bean的优先级
      * @return
      */
+
     @Primary
     @Bean(name = "ds1")
     @ConfigurationProperties(prefix = "spring.datasource.ds1")
-    public DataSource getDateSource1() {
-        log.info("ds1-----------------");
-        return DataSourceBuilder.create().build();
+    public DataSource getDateSource1(Environment env) {
+        //return DataSourceBuilder.create().build();
+        return getDataSource(env,"spring.datasource.ds1.","ds1");
     }
-
     @Bean(name = "ds2")
     @ConfigurationProperties(prefix = "spring.datasource.ds2")
-    public DataSource getDateSource2() {
-        log.info("ds2-----------------");
-        return DataSourceBuilder.create().build();
+    public DataSource getDateSource2(Environment env) {
+        //return DataSourceBuilder.create().build();
+        return getDataSource(env,"spring.datasource.ds2.","ds2");
+    }
+
+    public DataSource getDataSource(Environment env, String prefix, String dataSourceName){
+        Properties prop = build(env,prefix);
+        AtomikosDataSourceBean ds = new AtomikosDataSourceBean();
+        ds.setXaDataSourceClassName(MysqlXADataSource.class.getName());
+        ds.setUniqueResourceName(dataSourceName);
+        ds.setXaProperties(prop);
+        return ds;
+    }
+
+    protected Properties build(Environment env, String prefix) {
+        Properties prop = new Properties();
+        prop.put("url", env.getProperty(prefix + "jdbc_url"));
+        prop.put("user", env.getProperty(prefix + "username"));
+        prop.put("password", env.getProperty(prefix + "password"));
+        // prop.put("driverClassName", env.getProperty(prefix + "driverClassName", ""));
+        return prop;
+    }
+
+    @Bean(name = "sqlSessionFactory1")
+    public SqlSessionFactory sqlSessionFactory1(@Qualifier("ds1") DataSource dataSource)throws Exception {
+        return createSqlSessionFactory(dataSource);
+    }
+
+    @Bean(name = "sqlSessionFactory2")
+    public SqlSessionFactory sqlSessionFactory2(@Qualifier("ds2") DataSource dataSource)throws Exception {
+        return createSqlSessionFactory(dataSource);
     }
 
     /**
@@ -57,13 +88,25 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+    @Bean(name = "sqlSessionTemplate")
+    public CustomSqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory1")SqlSessionFactory factory1,@Qualifier("sqlSessionFactory2")SqlSessionFactory factory2) throws Exception {
+        Map<Object,SqlSessionFactory> sqlSessionFactoryMap = new HashMap<>();
+        sqlSessionFactoryMap.put("ds1",factory1);
+        sqlSessionFactoryMap.put("ds2",factory2);
+
+        CustomSqlSessionTemplate customSqlSessionTemplate = new CustomSqlSessionTemplate(factory1);
+        customSqlSessionTemplate.setTargetSqlSessionFactorys(sqlSessionFactoryMap);
+        customSqlSessionTemplate.setDefaultTargetSqlSessionFactory(factory1);
+        return customSqlSessionTemplate;
+    }
+
     /**
      * 将动态数据源注入到SqlSessionFactory
-     * @param dynamicDataSource
+     * @param
      * @return
      * @throws Exception
      */
-    @Bean(name = "SqlSessionFactory")
+    /*@Bean(name = "SqlSessionFactory")
     public SqlSessionFactory getSqlSessionFactory(@Qualifier("dynamicDataSource") DataSource dynamicDataSource)
             throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
@@ -71,6 +114,18 @@ public class DataSourceConfig {
         bean.setMapperLocations(
                 new PathMatchingResourcePatternResolver().getResources("classpath*:mapping/*.xml"));
         bean.setTypeAliasesPackage("com.boot.springmybatismultipledb.entity");
+        return bean.getObject();
+    }*/
+    private SqlSessionFactory createSqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setVfs(SpringBootVFS.class);
+        bean.setTypeAliasesPackage("com.boot.springmybatismultipledb.entity");
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session
+                .Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        bean.setConfiguration(configuration);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapping/*.xml"));
         return bean.getObject();
     }
 }
